70d7675c9e43cd2c12c20364a3de24402f9c2df5
SundaresanAI <ss@azentyk.ai>
2025-09-15 15:08:29 +0530

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -17,8 +17,8 @@ from urllib.parse import quote_plus
 # client = MongoClient(uri, tls=True, tlsAllowInvalidCertificates=False)
 
 # Initialize MongoDB client with TLS
-# client = MongoClient("mongodb://doctor-appointment-assistant-server:r05e2ZWM4DJrMGfEE02D8oJDFdhXp9ZCi57AqAECn4mval7SKosxhqVCVO80dtCu2Tkfr84ML0AyACDbykAtSw==@doctor-appointment-assistant-server.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@doctor-appointment-assistant-server@",tls=True, tlsAllowInvalidCertificates=False)
-client = MongoClient("mongodb+srv://azentyk:azentyk123@cluster0.b9aaq47.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb://doctor-appointment-assistant-server:r05e2ZWM4DJrMGfEE02D8oJDFdhXp9ZCi57AqAECn4mval7SKosxhqVCVO80dtCu2Tkfr84ML0AyACDbykAtSw==@doctor-appointment-assistant-server.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@doctor-appointment-assistant-server@",tls=True, tlsAllowInvalidCertificates=False)
+# client = MongoClient("mongodb+srv://azentyk:azentyk123@cluster0.b9aaq47.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["patient_db"]
 # Collections
 patient_information_details_table_collection = db["patient_information_details_table"]
